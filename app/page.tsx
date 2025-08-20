bd174c5734b0be189c07f4ccd531578a5b0f8e3a
CristinaJung <cristijung@gmail.com>
2025-08-20 14:05:53 -0300

    inicial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <section>
+        <h1>Redux - RTKs</h1>
+      </section>
+      <Footer />
+    </>
+  );
+}
