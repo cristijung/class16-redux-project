602306e6138e4bf51f2c1df0e076d5d23c0b8cc6
CristinaJung <cristijung@gmail.com>
2025-08-21 10:52:36 -0300

    estilização do page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 import Counter from "./components/counter/Counter";
 
-
 export default function Home() {
   return (
     <>
-      
-      <section className="container">
-        <h1>Redux - RTKs</h1>
+      <section className="container mx-auto p-4 md:p-8">
+        <h1>Redux - RTK</h1>
         <h2>Como trabalhar com Redux</h2>
-        <Counter/>
+        <hr className="my-10 h-1 bg-gray-600 border-0 rounded" />
+        <Counter />
+        <hr className="my-10 h-1 bg-gray-600 border-0 rounded" />
       </section>
-     
     </>
   );
 }
