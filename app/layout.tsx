1f074d568759c78ebec50a87cab452ac9059127b
CristinaJung <cristijung@gmail.com>
2025-08-21 10:44:19 -0300

    ajuste

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ReduxProvider } from "./ReduxProvider"; // Importe o componente correto
+import { ReduxProvider } from "./ReduxProvider"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
